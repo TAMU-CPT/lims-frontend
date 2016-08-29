f5fd1e92b5c0438ee8ddb29d6ae8cb19c574bfb5
Eric Rasche <rasche.eric@gmail.com>
2016-08-29 15:52:28 +0000

    Replace gulp

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var FileLoader = require('file-loader');
+
+module.exports = {
+	entry: ["./src/Index.js", "./src/styles/main.scss"],
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'main.js',
+		publicPath: '/'
+	},
+	module: {
+		loaders: [
+			{ test: /\.(css|scss)$/, loader: ExtractTextPlugin.extract('css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass?sourceMap') },
+			{ test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel',   query: {presets: ['es2015']}},
+			{ test: /\.html$/, loader: 'html' },
+			{ test: /\.(png|woff|woff2|eot|ttf|svg|json)$/, loader: 'url-loader?limit=100000'}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('[name].css', {
+			allChunks: true
+		})
+	]
+};
