a0fe275521332abf25be8c4e14173963c469546b
Eleni Mijalis <elenimijalis@gmail.com>
2016-12-01 20:43:50 -0600

    starting autocomplete functionality

--- js/ctrl/storage/add.js
+++ js/ctrl/storage/add.js
@@ -1,5 +1,19 @@
 export default function(base) {
-	base.controller("StorageAddCtrl", ["$scope", "$location", "$routeParams", "Restangular",
-		function($scope, $location, $routeParams, Restangular) {
-		}]);
+    base.controller("StorageAddCtrl", ["$scope", "$location", "$routeParams", "Restangular",
+        function($scope, $location, $routeParams, Restangular) {
+
+            $scope.ctrl = {
+                selectedItem: null,
+                searchText: null,
+                querySearch: function(queryString) {
+                    return Restangular.all("lims").customGET("storage", {room: queryString}).then(function(data) {
+                        return data.results;
+                    });
+                },
+                selectedItemChange: function(item) {
+                    console.log(item);
+                },
+            };
+
+        }]);
 }
