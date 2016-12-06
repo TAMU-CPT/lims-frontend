342ec9945dddbca30fd4a27192e387d147f3c1ad
Eleni Mijalis <elenimijalis@gmail.com>
2016-12-06 13:19:48 -0600

    https://github.com/angular/material/issues/3866

--- js/ctrl/storage/add.js
+++ js/ctrl/storage/add.js
@@ -1,7 +1,6 @@
 export default function(base) {
     base.controller("StorageAddCtrl", ["$scope", "$location", "$routeParams", "Restangular",
         function($scope, $location, $routeParams, Restangular) {
-
             $scope.room = {
                 selectedItem: null,
                 searchText: null,
@@ -21,20 +20,23 @@ export default function(base) {
                 searchText: null,
                 querySearch: function(queryString) {
                     return Restangular.all("lims").customGET("storage", {room: $scope.room.searchText, type: $scope.container.type}).then(function(data) {
-                        console.log(queryString, $scope.room.searchText, $scope.container.type);
                         return data.results;
                     });
                 },
                 selectedItemChange: function(item) {
-                    //console.log(item);
+                },
+                reset: function() {
+                    $scope.container.searchText = null;
+                    $scope.container.selectedItem = null;
                 },
             };
 
             $scope.$watch('room.searchText', function(newValue, oldValue) {
-                $scope.container.querySearch($scope.container.searchText);
+                $scope.container.reset();
             });
             $scope.$watch('container.type', function(newValue, oldValue) {
-                $scope.container.querySearch($scope.container.searchText);
+                $scope.container.reset();
             });
+
         }]);
 }
