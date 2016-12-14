3161957712d63419ad1f7ce05289ccbf88728ef3
Eleni Mijalis <elenimijalis@gmail.com>
2016-12-14 17:50:30 -0600

    sample type filteringg

--- js/ctrl/storage/inventory.js
+++ js/ctrl/storage/inventory.js
@@ -3,22 +3,26 @@ export default function(base) {
 	    function($scope, $location, $routeParams, Restangular) {
 
             $scope.sample_label;
+            $scope.choice = {
+                sample_category: ''
+            }
 
             $scope.go = function(id) {
                 $location.path("/storage/" + id); ;
             };
 
-             $scope.ordering="sample_label";
+            $scope.ordering="sample_label";
+            $scope.sample_categories = ['lysate', 'phagednaprep', 'envsample']
 
             $scope.updateData = function(page) {
                 if(!isNaN(parseInt(page))) {
                     $scope.query.page = page;
                 }
                 $scope.query.sample_label = $scope.sample_label;
-                 $scope.query.ordering = $scope.ordering;
+                $scope.query.sample_category = $scope.choice.sample_category;
+                $scope.query.ordering = $scope.ordering;
                 $scope.promise = Restangular.all("lims/storage").getList($scope.query).then(function(data) {
                     $scope.data = data;
-                    console.log($scope.data[0]);
                 });
             };
 
@@ -32,6 +36,7 @@ export default function(base) {
                 page: 1,
                 sample_label: null,
                 ordering: $scope.ordering,
+                sample_category: $scope.choice.sample_category,
             };
 
             $scope.updateData(1);
