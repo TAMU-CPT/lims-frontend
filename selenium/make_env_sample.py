fb1a16baa3fb84e856df9f6e8d62a7d8627077e0
Eleni Mijalis <elenimijalis@gmail.com>
2017-01-05 00:04:44 -0600

    identified login inputs/button

--- selenium/make_env_sample.py
+++ selenium/make_env_sample.py
@@ -1,6 +1,11 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
+
+# set up virtualenv
+# activate venv
+# create user
 
 class LimsEnvSampleCreate(unittest.TestCase):
 
@@ -12,6 +17,12 @@ class LimsEnvSampleCreate(unittest.TestCase):
         driver.get("http://localhost:10000/#/login")
         # driver.implicitly_wait(2)
         login_button = driver.find_element_by_xpath("//md-card-content/button")
+        username = driver.find_element_by_xpath("//input[@name='name']")
+        password = driver.find_element_by_xpath("//input[@name='password']")
+        username.send_keys("user") # set up above
+        password.send_keys("password") # set up above
+        login_button.click()
+        time.sleep(5)
 
     def tearDown(self):
         self.driver.close()
