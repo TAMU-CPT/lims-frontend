281f1bc7912f9160b3d2e679627296c6581bbd8b
Eric Rasche <rasche.eric@gmail.com>
2017-01-30 19:18:20 +0000

    lvoe for bact route

--- js/ctrl/bacteria/detail.js
+++ js/ctrl/bacteria/detail.js
@@ -5,8 +5,62 @@
 export default function(base) {
 	base.controller("BacteriaDetailCtrl", ["$scope", "$location", "$routeParams", "Restangular",
 		function($scope, $location, $routeParams, Restangular) {
+
+			$scope.bact = {
+				data: {},
+				disabled: true,
+
+				// Start editing
+				edit_data: function() {
+					$scope.bact.disabled = false;
+				},
+
+				// Finish editing
+				edit_data_save: function() {
+					$scope.bact.data.save().then(function(resp){
+						$scope.bact.disabled = true;
+					})
+				},
+
+				edit_data_cancel: function() {
+					$scope.bact.disabled = true;
+				},
+			};
+
+			// Phage List / Search
+			$scope.updateData = function(page) {
+				if(!isNaN(parseInt(page))) {
+					$scope.query.page = page;
+				}
+				// uncomment if ordering filter is implemented in backend
+				$scope.query.ordering = $scope.ordering;
+				$scope.query.name = $scope.search_input;
+
+				$scope.promise = Restangular.all("lims/phages").getList($scope.query).then(function(data) {
+					$scope.data = data;
+				});
+			};
+
+			$scope.options = {
+				limitSelect: true,
+				pageSelect: true,
+			};
+
+			$scope.query = {
+				limit: 10,
+				page: 1,
+				// uncomment if ordering filter is implemented in backend
+				ordering: $scope.ordering,
+				name: $scope.search_input,
+				host: $routeParams.bacteriaID,
+			};
+
+			$scope.updateData(1);
+
+
 			Restangular.one("lims/bacterias", $routeParams.bacteriaID).get().then(function(data) {
-				$scope.bacteria = data;
+				$scope.bact.data = data;
 			});
+
 		}]);
 }
